dbd3534474502b8f8cecc5ea117c52006b7db4ca
Piotr Grochowski <piogrek@gmail.com>
2023-03-24 11:08:54 +0000

    initial skeleton

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
